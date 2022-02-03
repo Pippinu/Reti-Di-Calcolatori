323fd82a3179beccf5a7cbc3d604e622ee57a274
P1ppinu <iacono760@gmail.com>
2022-02-03 11:08:57 +0100

    Aggiunto Apidoc

---
+++ REST/5.apidoc/apidoc/api_project.js
@@ -0,0 +1,18 @@
+define({
+  "name": "example-basic",
+  "version": "0.1.0",
+  "description": "apiDoc basic example",
+  "template": {
+    "withCompare": false
+  },
+  "url": "http://localhost:8080",
+  "sampleUrl": "http://localhost:8080",
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2021-03-15T14:37:10.702Z",
+    "url": "https://apidocjs.com",
+    "version": "0.26.0"
+  }
+});
